7b42c6f649111351e6d85dc38b7d0dfc40e5f261
Ajeet Yadav <ajeetyadav164@gmail.com>
2023-09-29 22:14:40 +0530

    Changed updated code

---
+++ function_app.py
@@ -0,0 +1,26 @@
+import azure.functions as func
+import logging
+
+app = func.FunctionApp()
+
+
+@app.route(route="http_trigger", auth_level=func.AuthLevel.ANONYMOUS)
+def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info("Python HTTP trigger function processed a request.")
+
+    name = req.params.get("name")
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get("name")
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}.")
+    else:
+        return func.HttpResponse(
+            "Hello World.",
+            status_code=200,
+        )
